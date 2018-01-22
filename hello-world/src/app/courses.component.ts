9f914f50ba18b3c95ef9f2c8444b4abb6f0b150e
gareththomasnz <gareththomasnz@yahoo.com>
2018-01-22 17:27:18 +1300

    third commit

--- hello-world/src/app/courses.component.ts
+++ hello-world/src/app/courses.component.ts
@@ -1,34 +1,53 @@
-import {CoursesService} from './courses.service';
-import {Component } from '@angular/core';
+import {
+	CoursesService
+} from './courses.service';
+import {
+	Component
+} from '@angular/core';
 
-export class CoursesService{
-    getCourses(){
-        return ["Course1","Course2","Course3"];
-    }
+export class CoursesService {
+	getCourses() {
+		return ["Course1", "Course2", "Course3"];
+	}
 }
 
 @Component({
-    selector: 'courses',
-    template: `
+	selector: 'courses',
+	template: `
           <h2>{{Title}}</h2>
           <li *ngFor="let course of courses">
             {{ course }}
           </li>
+          <div (click)="onDivClicked()">
+            <button type="button" class="btn btn-primary btn-sm" [class.active]="isActive"
+              (click)="onSave($event)">
+              Save
+            </button>
+          </div>
           `
 })
 
-export class CoursesComponent{
-    title = "List of courses";
-    courses;
+export class CoursesComponent {
+	title = "List of courses";
+	courses;
+  isActive = true;
+	// getTitle(){
+	//     return this.title;
+	// }
+  
+  onDivClicked(){
+     console.log("div was clicked");
+  }
+  
+  onSave($event){
+    $event.stopPropagation();
+    
+    console.log("button was clicked", $event);
+  }
 
-    // getTitle(){
-    //     return this.title;
-    // }
-
-    constructor(){
-        let service = new CoursesService();
-        console.log("service:"+service.getCourses())
-        this.courses = service.getCourses();
-    }
+	constructor() {
+		let service = new CoursesService();
+		console.log("service:" + service.getCourses())
+		this.courses = service.getCourses();
+	}
 }
-
