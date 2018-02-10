ab96cab1763e191345b04146ca039ae4050a8bae
gareththomasnz <gareththomasnz@yahoo.com>
2018-02-10 20:04:27 +1300

    ninth commit

--- hello-world/src/app/app.component.ts
+++ hello-world/src/app/app.component.ts
@@ -1,22 +1,71 @@
-import { FavoriteChangedEventArgs } from './favorite.favorite.component';
-import { Component } from '@angular/core';
+import {
+	FavoriteChangedEventArgs
+} from './favorite.favorite.component';
+import {
+	Component
+} from '@angular/core';
 
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css']
+});
 
 
 export class AppComponent {
-  post = {
-    title:"Angular app",
-    isFavorite: true
+  
+  task = {
+    title: 'Review applications',
+    assignee: {
+      null
+    }
+    
   }
-  courses = [1, 2];
+  
+  canSave = true;
+  
+	post = {
+		title: "Angular app",
+		isFavorite: true
+	}
+	courses;
 
-  onFavoriteChange(eventArgs: {EventArgs: FavoriteChangedEventArgs}){
-    console.log("Favorite changed", eventArgs);
+	loadCourses() {
+		this.courses = [{
+			id: 1,
+			name: 'course1'
+		}, {
+			id: 2,
+			name: 'course2'
+		}, {
+			id: 3,
+			name: 'course3'
+		}];
+	}
+
+	viewMode = 'map';
+
+	onFavoriteChange(eventArgs: {
+		EventArgs: FavoriteChangedEventArgs
+	}) {
+		console.log("Favorite changed", eventArgs);
+	}
+
+
+  onAdd() {
+    this.courses.push({
+      id: 4,
+      name: 'course4'
+    });
+  }
+  
+  onRemove() {
+    this.courses.indexOf(course);
+    this.courses.splice(index, 1);
+  }
+  
+  trackCourse(index, course) {
+    return course ? course.id : undefined;
   }
 }
